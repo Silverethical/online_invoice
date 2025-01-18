53b21b15022f145be1b7b112ef985f42fe0d29fa
dofxo <kargat504@gmail.com>
2025-01-19 02:55:36 +0330

    feat: add buttons for factor footer
    add factor reset function

--- src/components/factorFooter/FactorFooter.tsx
+++ src/components/factorFooter/FactorFooter.tsx
@@ -1,8 +1,21 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 const FactorFooter = () => {
+  const handleFactorReset = () => {
+    // reset all forms
+    const forms = document.querySelectorAll("form");
+    forms.forEach((form) => form.reset());
+
+    // clear date picker
+    const datePickerInput = document.querySelector(
+      "input.rmdp-input",
+    ) as HTMLInputElement;
+
+    if (datePickerInput) datePickerInput.value = "";
+  };
+
   return (
-    <section className="container">
+    <section className="container flex flex-col gap-10">
       <TextField
         placeholder="توضیحات"
         className="w-full description-input"
@@ -16,6 +29,22 @@ const FactorFooter = () => {
           },
         }}
       />
+
+      <div className="button-wrapper flex justify-between gap-5">
+        <Button
+          variant="contained"
+          className="w-full !bg-red-600 !rounded-[10px]"
+          onClick={handleFactorReset}
+        >
+          ریست فاکتور
+        </Button>
+        <Button
+          variant="contained"
+          className="w-full !bg-sky-700 !rounded-[10px]"
+        >
+          ذخیره فاکتور
+        </Button>
+      </div>
     </section>
   );
 };
