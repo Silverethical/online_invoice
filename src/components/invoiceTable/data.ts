ce0331fe672aebf8e9992ec32d4f20e3b1a21175
Majid Kargar <majidkargar01@gmail.com>
2025-01-22 22:40:22 +0330

    Refactor: change initial state to be one empty object

--- src/components/invoiceTable/data.ts
+++ src/components/invoiceTable/data.ts
@@ -3,16 +3,9 @@ import { GridRowsProp } from "@mui/x-data-grid";
 export const initialRows: GridRowsProp = [
   {
     id: "1",
-    "product-name": "lebas",
-    quantity: "1",
-    price: "1000",
-    "total-amount": "1000",
-  },
-  {
-    id: "2",
-    "product-name": "لبنیات",
-    quantity: "1",
-    price: "1000",
-    "total-amount": "1000",
+    "product-name": "",
+    quantity: "",
+    price: "",
+    "total-amount": "",
   },
 ];
