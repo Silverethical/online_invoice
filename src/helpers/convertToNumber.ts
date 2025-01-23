67275734edc2e2ba810f8f5931377017517336e3
Majid Kargar <majidkargar01@gmail.com>
2025-01-23 12:07:47 +0330

    Refactor: Change the function to allow persian and arabic numbers

--- src/helpers/convertToNumber.ts
+++ src/helpers/convertToNumber.ts
@@ -1,8 +1,19 @@
 export const convertToNumber = (value: any): number => {
   if (typeof value === "number") return value;
+
   if (typeof value === "string") {
-    const numericValue = value.replace(/[^0-9.-]+/g, "");
+    const persianArabicToWestern = (str: string): string =>
+      str.replace(/[\u0660-\u0669\u06F0-\u06F9]/g, (char) =>
+        String(char.charCodeAt(0) - (char <= "\u0669" ? 0x0660 : 0x06f0)),
+      );
+
+    const numericValue = persianArabicToWestern(value).replace(
+      /[^0-9.-]+/g,
+      "",
+    );
+
     return Number(parseFloat(numericValue) || 0);
   }
+
   return 0;
 };
